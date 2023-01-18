882b091c344c71c0979806dcd3b960d0c29d0223
leonildourbano <122062631+leonildourbano@users.noreply.github.com>
2023-01-17 22:54:17 -0300

    só cadastyro

---
+++ AppSchool/Migrations/20230118014607_CriaDbsStudentGrade03.cs
@@ -0,0 +1,131 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AppSchool.Migrations
+{
+    /// <inheritdoc />
+    public partial class CriaDbsStudentGrade03 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Student_Grade_GradeId",
+                table: "Student");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "StudentName",
+                table: "Student",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<byte[]>(
+                name: "Photo",
+                table: "Student",
+                type: "varbinary(max)",
+                nullable: true,
+                oldClrType: typeof(byte[]),
+                oldType: "varbinary(max)");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "GradeId",
+                table: "Student",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Section",
+                table: "Grade",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "GradeName",
+                table: "Grade",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Student_Grade_GradeId",
+                table: "Student",
+                column: "GradeId",
+                principalTable: "Grade",
+                principalColumn: "GradeId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Student_Grade_GradeId",
+                table: "Student");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "StudentName",
+                table: "Student",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<byte[]>(
+                name: "Photo",
+                table: "Student",
+                type: "varbinary(max)",
+                nullable: false,
+                defaultValue: new byte[0],
+                oldClrType: typeof(byte[]),
+                oldType: "varbinary(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "GradeId",
+                table: "Student",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Section",
+                table: "Grade",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "GradeName",
+                table: "Grade",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Student_Grade_GradeId",
+                table: "Student",
+                column: "GradeId",
+                principalTable: "Grade",
+                principalColumn: "GradeId",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
